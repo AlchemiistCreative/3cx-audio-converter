667640fc720ce3eceee46f9fa32e62be02053025
AlchemiistCreative <replyaddr@gmail.com>
2020-12-28 21:58:11 +0100

    port 80 added

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.post('/', (req, res) => {
 
 });
 
-app.listen(3000, () => console.log('listening on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('listening on port 3000'));
 
 
 
